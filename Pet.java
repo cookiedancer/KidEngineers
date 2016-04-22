6cf7394fd060b5a9f3176f044ffef1f74ded089d
N3v3rMor3 <tristanwalker44@gmail.com>
2016-04-22 12:06:49 -0400

    Added files via upload
    this is from class

--- Pet.java
+++ Pet.java
@@ -3,8 +3,8 @@ import greenfoot.*;
 /**
  * Write a description of class Pet here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Tristan Walker 
+ * version 1
  */
 public class Pet extends MightyPet
 {
@@ -16,21 +16,20 @@ public class Pet extends MightyPet
     public Pet()
     {
         this.name = new String();
-        this.age=0;
+        this.age = 0;
         this.sound = new String("boing.wav");
-        this.isFriendly= false;
+        this.isFriendly = false;
         this.playPetSound();
     }
     public Pet(String petName, int petAge, String imageFileName, String soundFileName)
     {
-        this.name= petName;
-        this.age= petAge;
-        this.image= imageFileName;
+        this.name = petName;
+        this.age = petAge;
+        this.image = imageFileName;
         this.sound = soundFileName;
         this.isFriendly = false;
         setImage(this.image);
     }
-    
     /**
      * Act - do whatever the Pet wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -38,10 +37,9 @@ public class Pet extends MightyPet
     public void act() 
     {
         // Add your action code here.
-         
     } 
     public void playPetSound()
     {
-       Greenfoot.playSound(this.sound);
+        Greenfoot.playSound(this.sound);
     }
 }
